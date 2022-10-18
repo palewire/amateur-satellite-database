92292034fc53f9d22cdd3a962b2d07ab4e6b5030
palewire <b@palewi.re>
2022-10-18 09:22:16 -0700

    Integrate satnogs database to the mirror, merge it with amsat list

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-The amateur satellites in space. A machine-readable mirror of [JE9PEL's website](http://www.ne.jp/asahi/hamradio/je9pel/satslist.htm).
+The amateur satellites in space. A machine-readable mirror of [JE9PEL's website](http://www.ne.jp/asahi/hamradio/je9pel/satslist.htm) and the [SatNOGS](https://db.satnogs.org/satellites/) database.
 
 ## Features
 
 - 🪞 Mirrors existing sources
+- ⛓️ Link datasets
 - ♻️ Converts to alternative formats
 - 🧹 Cleans minor flaws
 - 🔪 Offers alternative slices
 
 ## Files
 
-Files are automatically updated via [GitHub Action](https://github.com/palewire/ham-satellite-database/actions) and published in the `data` directory.
+Files are automatically updated via [GitHub Action](https://github.com/palewire/amateur-satellite-database/actions) and published in the `data` directory.
 
 File | URL
 :--- | :--
-All frequencies | [all-frequencies.csv](https://raw.githubusercontent.com/palewire/ham-satellite-database/main/data/all-frequencies.csv), [all-frequencies.json](https://raw.githubusercontent.com/palewire/ham-satellite-database/main/data/all-frequencies.json)
-Active frequencies  | [active-frequencies.csv](https://raw.githubusercontent.com/palewire/ham-satellite-database/main/data/active-frequencies.csv), [active-frequencies.json](https://raw.githubusercontent.com/palewire/ham-satellite-database/main/data/active-frequencies.json)
+AMSAT All frequencies | [amsat-all-frequencies.csv](https://raw.githubusercontent.com/palewire/amateur-satellite-database/main/data/amsat-all-frequencies.csv), [amsat-all-frequencies.json](https://raw.githubusercontent.com/palewire/ham-satellite-database/main/data/amsat-all-frequencies.json)
+AMSAT Active frequencies  | [amsat-active-frequencies.csv](https://raw.githubusercontent.com/palewire/amateur-satellite-database/main/data/amsat-active-frequencies.csv), [amsat-active-frequencies.json](https://raw.githubusercontent.com/palewire/ham-satellite-database/main/data/amsat-active-frequencies.json)
+SatNOGS  | [satnogs.csv](https://raw.githubusercontent.com/palewire/amateur-satellite-database/main/data/satnogs.csv), [satnogs.json](https://raw.githubusercontent.com/palewire/ham-satellite-database/main/data/satnogs.json)
--- src/download.py
+++ src/download.py
@@ -2,8 +2,24 @@ import click
 import pandas as pd
 
 
-@click.command()
+@click.group()
 def cli():
+    """Download satellite data."""
+    pass
+
+
+@cli.command()
+def satnogs():
+    """Download data from JE9PEL's website."""
+    url = "https://db.satnogs.org/api/satellites.json"
+    df = pd.read_json(url)
+    df.norad_cat_id = df.norad_cat_id.astype(int, errors="ignore")
+    df.to_csv("./data/satnogs.csv", index=False)
+    df.to_json("./data/satnogs.json", orient="records", indent=2)
+
+
+@cli.command()
+def je9pel():
     """Download data from JE9PEL's website."""
     # Download the data
     url = "http://www.ne.jp/asahi/hamradio/je9pel/satslist.csv"
@@ -11,8 +27,8 @@ def cli():
 
     # Name the columns
     df.columns = [
-        "satellite",
-        "number",
+        "name",
+        "norad_id",
         "uplink",
         "downlink",
         "beacon",
@@ -28,14 +44,25 @@ def cli():
     # Lowercase all the statuses
     df.status = df.status.str.lower()
 
+    # Merge in SatNOGS ids
+    satnogs = pd.read_csv(
+        "./data/satnogs.csv", usecols=["sat_id", "norad_cat_id"], dtype=str
+    ).rename(columns={"sat_id": "satnogs_id", "norad_cat_id": "norad_id"})
+    satnogs = satnogs[~pd.isnull(satnogs.norad_id)].copy()
+    satnogs.norad_id = satnogs.norad_id.str.replace(".0", "", regex=False)
+    merged = df.merge(satnogs, on="norad_id", how="left")
+    assert len(merged) == len(df)
+
     # Write out
-    df.to_csv("./data/all-frequencies.csv", index=False)
-    df.to_json("./data/all-frequencies.json", orient="records", indent=2)
+    merged.to_csv("./data/amsat-all-frequencies.csv", index=False)
+    merged.to_json("./data/amsat-all-frequencies.json", orient="records", indent=2)
 
     # Filter to active, then write that out
-    active_df = df[df.status == "active"].drop(["status"], axis=1).copy()
-    active_df.to_csv("./data/active-frequencies.csv", index=False)
-    active_df.to_json("./data/active-frequencies.json", orient="records", indent=2)
+    active_df = merged[merged.status == "active"].drop(["status"], axis=1).copy()
+    active_df.to_csv("./data/amsat-active-frequencies.csv", index=False)
+    active_df.to_json(
+        "./data/amsat-active-frequencies.json", orient="records", indent=2
+    )
 
 
 if __name__ == "__main__":
